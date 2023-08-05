fdfa13b106cfa13fd0d1a02f92b3a3ea5ea799a3
sofy <bedlusofi@gmail.com>
2023-08-05 20:05:09 +0300

    task-5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+define hash_password function
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """
+    returns a salted, hashed password, which is a byte string
+    """
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_pwd
