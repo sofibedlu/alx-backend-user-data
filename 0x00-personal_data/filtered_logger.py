91d9108b0ca98ab0d379a4d7084303e9a5a07fc2
sofy <bedlusofi@gmail.com>
2023-08-05 15:25:11 +0300

    task-1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 define filter-datum
 """
 import re
+import logging
 from typing import List
 
 
@@ -15,3 +16,24 @@ def filter_datum(fields: List[str], redaction: str,
                          '{}={}'.format(field, redaction), message,
                          flags=re.IGNORECASE)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """intialize"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """return customized radicated log messages"""
+        formated = filter_datum(self.fields, self.REDACTION,
+                                record.msg, self.SEPARATOR)
+        record.msg = formated
+        return super().format(record)
