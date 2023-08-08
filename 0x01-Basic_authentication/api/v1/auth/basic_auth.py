24314f03629b1a62de41ae2a1811ecf5cfdaca0e
sofy <bedlusofi@gmail.com>
2023-08-08 10:00:33 +0300

    task-7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,19 @@ class BasicAuth(Auth):
     represents a basic authentication mechanism that extends the
     abstract Auth class.
     """
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Extract the credentials from the 'Authorization' header.
+        """
+        if authorization_header is None:
+            return None
+
+        if not type(authorization_header) is str:
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            return authorization_header[6:]
