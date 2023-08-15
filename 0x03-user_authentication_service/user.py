71a1b42e5e7f39b21ab4a9c1b1c56c289bce46f7
sofy <bedlusofi@gmail.com>
2023-08-15 20:28:24 +0300

    intial-commit

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module defines a SQLAlchemy data model for representing user data.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from typing import Optional
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Represents a user in the application.
+
+    Attributes:
+        id (int): Primary key identifying the user.
+        email (str): Email address of the user. Cannot be null.
+        hashed_password (str): Hashed password of the user. Cannot be null.
+        session_id (str): Session ID of the user. Can be null.
+        reset_token (str): Token used for password reset. Can be null.
+    """
+    __tablename__ = 'users'
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String, nullable=False)
+    hashed_password: str = Column(String, nullable=False)
+    session_id: Optional[str] = Column(String, nullable=True)
+    reset_token: Optional[str] = Column(String, nullable=True)
