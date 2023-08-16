aae64cca2eb50cfe34daebb22fb344c159e6d684
sofy <bedlusofi@gmail.com>
2023-08-16 16:40:50 +0300

    task-7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 Start Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+Auth = Auth()
 app = Flask(__name__)
 
 
@@ -15,6 +17,21 @@ def index():
     return welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    Create a new user via a POST request.
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        user = Auth.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
