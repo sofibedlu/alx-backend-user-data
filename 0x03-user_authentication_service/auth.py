9595d3b8fd2288024880fc18555f8cd533968636
sofy <bedlusofi@gmail.com>
2023-08-16 20:35:09 +0300

    update the user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,6 +62,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return _generate_uuid()
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return
