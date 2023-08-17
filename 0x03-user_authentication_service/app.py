ff101f04bff3e2d6944c694bbbb0ce3cf63c836b
sofy <bedlusofi@gmail.com>
2023-08-17 10:27:10 +0300

    task-15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,5 +71,18 @@ def logout():
         abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    Retrieve user profile based on the provided session ID.
+    """
+    session_id = request.cookies.get('session_id')
+    user = Auth.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
